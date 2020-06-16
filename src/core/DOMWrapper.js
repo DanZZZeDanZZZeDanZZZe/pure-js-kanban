8de40a33eb2c5add870e8d267c77c82c33f834af
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-06-17 01:06:00 +0300

    added DOM wrapper

---
+++ src/core/DOMWrapper.js
@@ -0,0 +1,35 @@
+const CONSTRUCTOR_ERROR =
+    'The parameter must be either a string or an HTML element'
+const CREATE_ERROR =
+    'The parameter must be either a string or an instance of Array'
+
+
+class DOMWrapper {
+  constructor(el) {
+    if (typeof el === 'string') {
+      this.el = document.querySelector(el.trim())
+      return this
+    }
+    if (el instanceof HTMLElement) {
+      this.el = el
+      return this
+    }
+    throw new Error(CONSTRUCTOR_ERROR)
+  }
+}
+
+export function $(el) {
+  return new DOMWrapper(el)
+}
+
+$.create = (tagName, classNames) => {
+  const el = document.createElement(tagName)
+  if (typeof classNames === 'string') {
+    classNames = classNames.trim().split(' ')
+  }
+  if (classNames instanceof Array) {
+    classNames.forEach(className => el.classList.add(className))
+    return $(el)
+  }
+  throw new Error(CREATE_ERROR)
+}
