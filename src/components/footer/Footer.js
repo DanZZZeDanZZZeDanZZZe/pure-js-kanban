5f65ca8ded56751f9398d47b2e1825e5681b790c
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-07-07 19:04:19 +0300

    added statistics in the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,15 +3,26 @@ import {AppComponent} from '@core'
 class Footer extends AppComponent {
   constructor() {
     super({
-      classNames: 'footer'
+      classNames: 'footer',
+      watch: ['cards']
     })
+
+    const {cards} = this.$appState
+    const last = cards.length - 1
+
+    this.activeTasks = cards
+        .slice(0, last)
+        .reduce((count, card) => {
+          return count + card.tasks.length
+        }, 0)
+    this.finishedTasks = cards[last].tasks.length
   }
 
   render() {
     return `
       <div class="footer-statistics">
-        <p>Active taslks: N</p>
-        <p>Finished tasks: N</p>
+        <p>Active taslks: ${this.activeTasks}</p>
+        <p>Finished tasks: ${this.finishedTasks}</p>
       </div>
       <div class="footer-user">
         <p>Name</p>
